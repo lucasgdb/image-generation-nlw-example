b657de780cfe073cef5f104df847db2c3c8b3624
lucasgdb <lucasgdbittencourt@gmail.com>
2022-11-24 02:54:08 -0300

    chore: make it totally independent from other resources & code refactored

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -7,58 +7,52 @@ export const config = {
   runtime: 'experimental-edge',
 };
 
-const fontRegular = fetch(new URL('../../assets/Roboto-Regular.ttf', import.meta.url)).then((res) => res.arrayBuffer());
-
-const fontBold = fetch(new URL('../../assets/Roboto-Bold.ttf', import.meta.url)).then((res) => res.arrayBuffer());
-
 export default async function handler(req: NextRequest) {
-  const robotoRegular = await fontRegular;
-  const robotoBold = await fontBold;
+  const robotoRegular = await (await fetch(new URL('../../assets/Roboto-Regular.ttf', import.meta.url))).arrayBuffer();
+  const robotoBold = await (await fetch(new URL('../../assets/Roboto-Bold.ttf', import.meta.url))).arrayBuffer();
 
   const { searchParams } = req.nextUrl;
 
   const githubUsername = searchParams.get('githubUsername');
   const cardType = searchParams.get('cardType') ?? 'explorer';
 
+  const cardUrl =
+    cardType === 'explorer'
+      ? `${process.env.NEXT_PUBLIC_URL}/bg-explorer.png`
+      : `${process.env.NEXT_PUBLIC_URL}/bg-ignite.png`;
+
   const getContent = async () => {
-    if (githubUsername) {
-      const response = await fetch(`https://api.github.com/users/${githubUsername}`, {
-        headers: {
-          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-          Accept: 'application/vnd.github+json',
-        },
-      });
+    if (!githubUsername) {
+      const imageUrl = searchParams.get('imageUrl') ?? '';
+      const username = searchParams.get('username') ?? '';
+      const lastname = searchParams.get('lastname') ?? '';
 
-      const data = await response.json();
-
-      const userExists = data?.message?.toLowerCase() !== 'not found';
-
-      return {
-        imageUrl: data.avatar_url ?? `${process.env.NEXT_PUBLIC_URL}/404.png`,
-        username: data.name ?? '',
-        lastname: userExists ? githubUsername : '',
-      };
+      return { imageUrl, username, lastname };
     }
 
-    const imageUrl = searchParams.get('imageUrl') ?? '';
-    const username = searchParams.get('username') ?? '';
-    const lastname = searchParams.get('lastname') ?? '';
+    const response = await fetch(`https://api.github.com/users/${githubUsername}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        Accept: 'application/vnd.github+json',
+      },
+    });
 
-    return { imageUrl, username, lastname };
+    const data = await response.json();
+
+    const userExists = data?.message?.toLowerCase() !== 'not found';
+
+    return {
+      imageUrl: data.avatar_url ?? `${process.env.NEXT_PUBLIC_URL}/404.png`,
+      username: data.name ?? '',
+      lastname: userExists ? githubUsername : '404',
+    };
   };
 
   const { imageUrl, username, lastname } = await getContent();
 
-  const cardUrl = cardType === 'explorer' ? 'https://i.imgur.com/etPVtuz.png' : 'https://i.imgur.com/XVJkpAi.png';
-
   return new ImageResponse(
     (
-      <div
-        tw="flex flex-col bg-transparent rounded-xl w-full h-full"
-        style={{
-          backgroundImage: `url(${cardUrl})`,
-        }}
-      >
+      <div tw="flex flex-col bg-transparent rounded-xl w-full h-full" style={{ backgroundImage: `url("${cardUrl}")` }}>
         <div tw="flex self-center w-[150px] h-[150px] border-solid border-4 border-[#f7dd43] rounded-full overflow-hidden mt-[100px]">
           {imageUrl && <img src={imageUrl} alt="avatar" tw="w-full h-full" />}
         </div>
